a730ddb0c5fca623e5118629e6642c0c4820eed1
zenix <ze9ix@live.no>
2017-11-27 12:24:30 +0100

    initial commit

---
+++ devopsexam/src/main/kotlin/org/zenix/devopsexam/DevopsexamApplication.kt
@@ -0,0 +1,11 @@
+package org.zenix.devopsexam
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DevopsexamApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DevopsexamApplication::class.java, *args)
+}
