e07a2647bdbdf128cc46b42d5618f5924a471dd0
zenix <ze9ix@live.no>
2017-11-30 13:19:33 +0100

    More quotes

--- devopsexam/src/main/kotlin/org/zenix/devopsexam/QuoteService.kt
+++ devopsexam/src/main/kotlin/org/zenix/devopsexam/QuoteService.kt
@@ -14,7 +14,10 @@ class QuoteService {
     private final fun createRandomQuoteDispenser(): RandomElementDispenser<String> {
         // http://www.junauza.com/2010/12/top-50-programming-quotes-of-all-time.html
         return RandomElementDispenser(
-                mutableListOf("Talk is cheap. Show me the code. ― Linus Torvalds",
+                mutableListOf(
+                        "In My Egotistical Opinion, most people's C programs should be indented six feet downward and covered with dirt. - Blair P. Houghton",
+                        "Measuring programming progress by lines of code is like measuring aircraft building progress by weight - Bill Gates",
+                        "Talk is cheap. Show me the code. ― Linus Torvalds",
                         "Walking on water and developing software from a specification are easy if both are frozen.- Edward V Berard",
                         "I think Microsoft named .Net so it wouldn’t show up in a Unix directory listing. - Oktal",
                         "I don't care if it works on your machine! We are not shipping your machine! - Vidiu Platon."
